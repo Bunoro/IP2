a3ce1ca9569b1aca1f1310985e5051bd16113285
Bunoro <raphsonl@gmail.com>
2020-07-23 10:34:16 +0300

    add function to show month can't be less than 1 or great than 12

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,27 @@ function getAkanName() {
     let monthOfBirth = Number(document.getElementById("month-input").value)
     let dateOfBirth = Number(document.getElementById("date-input").value)
     let genders = document.getElementsByName("gender")
+
+    //function to generate gender value
+    function getGender(){
+        for (let gender of genders)
+        if (gender.checked){
+            return gender.value;
+        }
+    }
+}
+
+let myGenderValue = getGender ();
+console.log(myGenderValue);
+
+//functions to validate values
+function monthValidator(params) {
+    if (monthOfBirth<1 || monthOfBirth>12) {
+        return false;
+    } else {
+        return true;
+    }
+        
+    }
+    
 }
