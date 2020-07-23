7235a7d0ef7a2215b66c050032e6034ed0ffc6aa
Bunoro <raphsonl@gmail.com>
2020-07-23 08:01:39 +0300

    add function in js to get d.o.b values

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function getAkanName() {
+    let yearOfBirth = document.getElementById("year-input").value
+    let monthOfBirth = Number(document.getElementById("month-input").value)
+    let dateOfBirth = Number(document.getElementById("date-input").value)
+    let genders = document.getElementsByName("gender")
+}
