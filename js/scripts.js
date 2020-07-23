39fdc4a4fd8b305625f38c671236a6939cdf03ce
Bunoro <raphsonl@gmail.com>
2020-07-23 17:41:40 +0300

    ammend scripts.js at line 51 so as to display name when date is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,65 @@
-function compute (event) {
+function compute(event) {
     console.log(event);
     event.preventDefault();
     
 
-    var year=document.getElementById("year-input").value;
-    var month=document.getElementById("month-input").value;
-    var date=document.getElementById("date-input").value;
+    var year=document.getElementById("year").value;
+    var month=document.getElementById("month").value;
+    var day=document.getElementById("day").value;
     var gender=document.getElementById("gender").value;
-    var form=new FormData (event.target);
-    var year=form.get("year-input");
-    var month=form.get("month-input");
-    var date=form.get("date-input");
+    var form=new FormData(event.target);
+    var year=form.get("year");
+    var month=form.get("month");
+    var day=form.get("day");
     var gender=form.get("gender");
-    var day=[year-input, month-input, date-input];
-    var day=new Date(date);
+    var akanNames = {
+        Sunday: {
+        male: 'Kwasi',
+        female: 'Akosua'
+        },
+        Monday: {
+        male: 'Kwadwo',
+        female: 'Adwoa'
+        },
+        Tuesday: {
+        male: 'Kwabena',
+        female: 'Abenaa'
+        },
+        Wednesday: {
+        male: 'Kwaku',
+        female: 'Akua'
+        },
+        Thursday: {
+        male: 'Yaw',
+        female: 'Yaa'
+        },
+        Friday: {
+        male: 'Kofi',
+        female: 'Afua'
+        },
+        Saturday: {
+        male: 'Kwame',
+        female: 'Ama'
+        },
+        }
+    var date=[year, month, day];
+    var date=new Date(date);
     console.log(date);
-
+    var dateBorn = date.getDay();
+    console.log(dateBorn);
+    var dayOfTheWeek = Object.keys(akanNames);
+    var dayName = akanNames[dayOfTheWeek[dateBorn]][gender];
+        console.log(dayName);
+    
+    if (gender==="male" && year>=1800 && month>=1 && month<13 && day>=1 && day<32){
+        alert("On this day of  " + dayOfTheWeek[date.getDay()]+ " " + "you were born and your Akan name is " + dayName);
+    }
+    else if (gender==="female" && year>=1800 && month>=1 && month<=12 && day>=1 && day<=31){
+        alert("On this day of  " + dayOfTheWeek[date.getDay()]+ " " + "you were born and your Akan name is " + dayName);
+    }
+    else {
+        alert("You have input the wrong date. Please try again!");
+    }
 
 
 }
